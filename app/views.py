898196c4a1fe14d2aabb17db3e1460e11c44248e
Trinity John <trinityrtime@gmail.com>
2020-01-30 14:43:25 +0300

    config;control flow;control block with if statement

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from flask import render_template
-from flask import app
+from app import app
 
 # #views
 # @app.route('/')
@@ -19,4 +19,13 @@ def movie(movie_id):
     View movie page function that returns the movie details page and its data
     '''
     return render_template('movie.html',id = movie_id)
+
+def index():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+
+    title = 'Home - Welcome to The best Movie Review Website Online'
+    return render_template('index.html', title = title)
     
