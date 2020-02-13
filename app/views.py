2d7599f374c3ac79238fc08375a8edc9f8359305
Trinity John <trinityrtime@gmail.com>
2020-02-13 10:05:23 +0300

    more items to display

--- app/views.py
+++ app/views.py
@@ -21,12 +21,18 @@ def movie(movie_id):
     '''
     return render_template('movie.html',id = movie_id)
 
+@app.route('/')
 def index():
 
     '''
     View root page function that returns the index page and its data
     '''
-
+    # Getting popular movie
+    popular_movies = get_movies('popular')
+    upcoming_movie = get_movies('upcoming')
+    now_showing_movie = get_movies('now_playing')
+    print(popular_movies)
     title = 'Home - Welcome to The best Movie Review Website Online'
-    return render_template('index.html', title = title)
+    #return render_template('index.html', title = title, popular=popular_movies)
+    return render_template('index.html', title = title, popular = popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie )
     
